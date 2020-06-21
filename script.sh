54370cd975f1758ffef28849a41ca869b3d07511
Dheemanth-gr <47374416+Dheemanth-gr@users.noreply.github.com>
2020-06-21 11:52:28 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,6 +2,6 @@ lex Convert.l
 gcc lex.yy.c
 ./a.out < guess.txt > guess.json
 python3 Tree.py > output.json
-#node getButtonQuerySelectors.js > queries.txt 
-#python3 autoscript.py
-rm a.out lex.yy.c guess.json #output.json queries.txt
+node getButtonQuerySelectors.js > queries.txt 
+python3 autoscript.py
+rm a.out lex.yy.c guess.json output.json queries.txt
